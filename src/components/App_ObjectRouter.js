5c781fc757b6c09f7ca5d5180081176edc0d0de8
John Niduelan <jeniduelan@gmail.com>
2024-09-21 11:01:26 +1000

    feat: project reorganized, navbar with navlinks

---
+++ src/components/App_ObjectRouter.js
@@ -0,0 +1,32 @@
+
+import { createBrowserRouter, RouterProvider} from 'react-router-dom';
+import './App.css';
+import HomePage from './pages/HomePage';
+import ContactPage from './pages/ContactPage';
+import AboutPage from './pages/AboutPage';
+
+// Convert an array of objects into <Routes> component containing <Route> components
+const someFancyRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />
+  },
+  {
+    path: "/about",
+    element: <AboutPage />
+  },
+  {
+    path: "/contact",
+    element: <ContactPage />
+  }
+]);
+
+function App() {
+  return (
+    <div className="App">
+      <RouterProvider router={someFancyRouter} />
+    </div>
+  );
+}
+
+export default App;
